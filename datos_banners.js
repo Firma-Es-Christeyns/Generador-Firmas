f42f6cd20b06d0262e161b2290b09945576ec122
AnaEstruch <ana.estruch@christeyns.com>
2025-10-01 13:20:45 +0200

    Update datos_banners.js

--- datos_banners.js
+++ datos_banners.js
@@ -2,8 +2,9 @@
 // El equipo de Marketing solo debe editar las URLs que están dentro de las comillas.
 
 const bannerLinks = {
-  "general": "https://www.christeyns.com/es-es/SHI/",
+  "general": "https://www.christeyns.com/es-es/article/christeyns-hygienalia-servicio-higiene-integral-shi/",
   "laundry": "https://www.christeyns.com/es-es/soluciones/ahorro-de-agua/hydro/",
-  "ph": "https://www.christeyns.com/es-es/SHI/",
+  "ph": "https://www.christeyns.com/es-es/article/christeyns-hygienalia-servicio-higiene-integral-shi/",
   "food": "https://www.christeyns.com/es-es/SHI/"
 };
+
